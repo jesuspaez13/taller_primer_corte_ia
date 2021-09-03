cf6ada3e0ca2f39b1d8c0abbff11c92715972364
Jesus Paez <jpaez13@cuc.edu.co>
2021-09-03 16:08:50 -0500

    Ejercicio 1

--- ejercicios.py
+++ ejercicios.py
@@ -5,3 +5,15 @@ Created on Fri Sep  3 15:59:28 2021
 @author: USER
 """
 
+# Ejercicio 1
+#  y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
+parentesis = 5 + 2 - 5
+parentesis_al_cuadrado = parentesis ** 2
+division = 8 / 2
+operacion = 5 + division - 30
+parentesis_total = parentesis_al_cuadrado * operacion
+dos_por_cinco_menos_tres = 2 * 5 - 3
+division_total = parentesis_total / dos_por_cinco_menos_tres
+print(f'El resultado de la operacion es: {division_total}')
+
+
